e88b17f25c277db44d4a5dbb801a899cd2f8b299
Treehatshrimp <jasonjiang000@gmail.com>
2026-03-20 19:39:20 -0500

    767 Keeping _Hero on Screen
    Where i stopped at

---
+++ Assets/_Scripts/Shield.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Dynamic")]
+    public int levelShown = 0; //Set between lines
+
+    //Non-public variable not appear in Inspector
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     mat = GetComponent<Renderer>().material;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Read the current shield level from the Hero Singleton
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        //if different from levelShown
+        if(levelShown != currLevel)
+        {
+            levelShown = currLevel;
+            //Adjust texture offset to shwo different shield level
+            mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0);
+        }
+        //Rotate the shield a bit every frame in a time-based way
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+    }
+}
