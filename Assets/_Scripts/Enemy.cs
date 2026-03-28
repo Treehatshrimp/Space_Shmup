8d3e9460b1da59b5417d46c9996867730a7a1fb3
DinkyKinky <cosmicthedutchie@gmail.com>
2026-03-27 22:40:44 -0500

    Finished revising the enemy onCollisionEnter method

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     [Header("Inscribed")]
     public float speed = 10f; //Movement
     public float fireRate = 0.3f; //Seconds/shot
-    public float health = 10; 
+    public float health = 10;
     public int score = 100; //Points earned for destroying
 
     protected BoundsCheck bndCheck;
@@ -29,7 +29,7 @@ public class Enemy : MonoBehaviour
             this.transform.position = value;
         }
     }
-    void Update ()
+    void Update()
     {
         Move();
         //Check whether this Enemy has gone off the bottom of the screen
@@ -47,23 +47,51 @@ public class Enemy : MonoBehaviour
         }
         */
     }
-    public virtual void Move ()
+    public virtual void Move()
     {
         Vector3 tempPos = pos;
         tempPos.y -= speed * Time.deltaTime;
         pos = tempPos;
     }
+    // private void OnCollisionEnter(Collision coll)
+    // {
+    //     GameObject otherGO = coll.gameObject;
+    //     if(otherGO.GetComponent<ProjectileHero>() != null)
+    //     {
+    //         Destroy (otherGO); //Destroy Projectile
+    //         Destroy(gameObject); //Destory this Enemy GameObject
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Enemy hit by non-ProjectileHero: " + otherGO.name);
+    //     }
+    // }
+
     private void OnCollisionEnter(Collision coll)
     {
         GameObject otherGO = coll.gameObject;
-        if(otherGO.GetComponent<ProjectileHero>() != null)
+
+        // check for collision with projectile hero
+        ProjectileHero p = otherGO.GetComponent<ProjectileHero>();
+        if (p != null)
         {
-            Destroy (otherGO); //Destroy Projectile
-            Destroy(gameObject); //Destory this Enemy GameObject
+            // only damage this enemy if its on screen
+            if (bndCheck.isOnScreen)
+            {
+                // get the damage ammount from the main weap dict
+                health -= Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
+                if (health <= 0)
+                {
+                    // destory this enemy
+                    Destroy(this.gameObject);
+                }
+            }
+            // destory the porjectilehero reguardless
+            Destroy(otherGO);
         }
         else
         {
-            Debug.Log("Enemy hit by non-ProjectileHero: " + otherGO.name);
+            print("Enemy hit by non-ProjectileHero " + otherGO.name);
         }
     }
 }
