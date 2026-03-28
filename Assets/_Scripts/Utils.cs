274b1bf1367e793836210ec565a10eb7083a6187
DinkyKinky <cosmicthedutchie@gmail.com>
2026-03-27 22:44:30 -0500

    added Materials Function to utils

--- Assets/_Scripts/Utils.cs
+++ Assets/_Scripts/Utils.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Utils : MonoBehaviour
 {
-    // == Bezier Curves =============================== \\
+    // == Bezier Curves ============================================================== \\
     /// <summary>
     /// While most Bezier curves are 3 or 4 points, it is possible to have
     /// any number of points using this recursive function.
@@ -34,4 +34,22 @@ public class Utils : MonoBehaviour
         // when complete, vArr[0,0] holds the final interpolated value
         return vArr[0, 0];
     }
+
+    // == Materials Function ============================================================== \\
+    /// <summary>
+    /// Returns a list of all materials on this gameoject and its children
+    /// </summary>
+    /// <param name="go">The GameObject on which to search for Renderers</param>
+    static public Material[] GetAllMaterials(GameObject go)
+    {
+        Renderer[] rends = go.GetComponentsInChildren<Renderer>();
+
+        Material[] mats = new Material[rends.Length];
+        for (int i = 0; i < rends.Length; i++)
+        {
+            mats[i] = rends[i].material;
+        }
+        return mats;
+    }
+
 }
